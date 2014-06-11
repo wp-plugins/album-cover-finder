54840a415840ed0d0dd680b356ab5170d0901660
urre <urre@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-06-11 07:26:50 +0000

    tagging version 0.32
    git-svn-id: https://plugins.svn.wordpress.org/album-cover-finder/tags/0.32@930129 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- js/admin.js
+++ js/admin.js
@@ -13,6 +13,7 @@
 			} else if(el.val() == 15) {
 				size.html('Large');
 			}
+
 		});
 
 		// Disble clear button at first
@@ -41,10 +42,12 @@
 
 			// Variables to send via Ajax
 			var attachment_id = $(this).parent().parent().find('img').data('lfm_fileid'),
-			post_id       = $('#pid').text();
+			post_id           = $('#pid').text();
+
+			console.log(attachment_id);
 
 			$.post(
-				albumcoverfinder.ajaxurl,	{
+				AlbumCoverFinderParams.ajax_url,	{
 					action: 'and_action',
 					the_attachment: attachment_id,
 					the_post: post_id,
@@ -52,10 +55,9 @@
 
 					// Build image for attaching to DOM
 					var chosen_image = '<img width="100" height="100" src="'+data+'" class="attachment-post-thumbnail" style="display:block;">';
-					var chosen_image_with_link = '<a title="Change featured image" href="'+albumcoverfinder.uploadurl+'?post_id='+post_id+'&amp;type=image&amp;TB_iframe=1&amp;width=640&amp;height=375" id="set-post-thumbnail" class="thickbox">'+chosen_image+'</a>';
+					var chosen_image_with_link = '<a title="Change featured image" href="'+AlbumCoverFinderParams.uploadurl+'?post_id='+post_id+'&amp;type=image&amp;TB_iframe=1&amp;width=640&amp;height=375" id="set-post-thumbnail" class="thickbox">'+chosen_image+'</a>';
 					$('#postimagediv .inside').prepend(chosen_image_with_link);
 					$('#postimagediv .inside p').hide();
-
 
 				});
 
@@ -84,7 +86,7 @@
 		$('#findalbum').on('click', function(e) {
 
 			$('#wait').show();
-			$('#findalbum').val(prefix_object_name.searching);
+			$('#findalbum').val(AlbumCoverFinderParams.searching);
 
 			var artist = $('#query_artist').val(),
 			album      = $('#query_album').val(),
@@ -98,24 +100,27 @@
 		$('#setattachment').on('click', function(e) {
 
 			$('#wait').show();
-			// Variables for sending via Ajax
-			var att_count = $('.countattachments').text().substr(0,1),
-			img_url = $('#theimgurl').text(),
+
+			var
+			att_count     = $('.countattachments').text().substr(0,1),
+			img_url       = $('#theimgurl').text(),
 			attachment_id = $(this).data('idnr'),
-			pid = $('#pid').text();
+			pid           = $('#pid').text();
 
 			// Set number of attachments
-			$('.countattachments').text(parseInt(att_count)+1+' '+prefix_object_name.files);
+			$('.countattachments').text(parseInt(att_count)+1+' '+AlbumCoverFinderParams.files);
 
 			// Ajax request, set post post thumbnail
 			$.post(
-				albumcoverfinder.ajaxurl,	{
+				AlbumCoverFinderParams.ajax_url,	{
 					action: 'and_action',
 					setattachment: img_url,
 					postid: pid,
 				},	function(data) {
 
-						$('.lfm_attachments').prepend('<div class="lfm_file cf"><img data-lfm_fileid="'+data+'" src="'+img_url+'"><div class="lfm_text"><a class="setpostthumbnail button" value="'+prefix_object_name.set+'">'+prefix_object_name.set+'</a><a href="#" class="lfm_detach_attachment">'+prefix_object_name.remove+'</a></div></div>');
+					console.log(data);
+
+						// $('.lfm_attachments').prepend('<div class="lfm_file cf"><img data-lfm_fileid="'+pid+'" src="'+img_url+'"><div class="lfm_text"><a class="setpostthumbnail button" value="'+AlbumCoverFinderParams.set+'">'+AlbumCoverFinderParams.set+'</a><a href="#" class="lfm_detach_attachment">'+AlbumCoverFinderParams.remove+'</a></div></div>');
 						$('#wait').hide();
 				});
 
@@ -124,12 +129,12 @@
 		$('.lfm_viewattachments').on('click', function(e) {
 
 			// View attached attachments
-			if($(this).text() === prefix_object_name.view) {
-				$('.lfm_attachments').slideDown(100);
-				$(this).text(prefix_object_name.hide);
+			if($(this).text() === AlbumCoverFinderParams.view) {
+				$('.lfm_attachments').show();
+				$(this).text(AlbumCoverFinderParams.hide);
 			} else {
-				$('.lfm_attachments').slideUp(100);
-				$(this).text(prefix_object_name.view);
+				$('.lfm_attachments').hide();
+				$(this).text(AlbumCoverFinderParams.view);
 			}
 			e.preventDefault();
 
@@ -146,11 +151,11 @@
 
 			// Update number
 			var att_count = $('.countattachments').text().substr(0,1);
-			$('.countattachments').text(parseInt(att_count)-1+' '+prefix_object_name.files+'');
+			$('.countattachments').text(parseInt(att_count)-1+' '+AlbumCoverFinderParams.files+'');
 
 			// Ajax request, detach attachment from post
 			$.post(
-				albumcoverfinder.ajaxurl,	{
+				AlbumCoverFinderParams.ajax_url,	{
 					action: 'and_action',
 					detachattachment: attachment_id,
 				},	function(data) {
@@ -172,16 +177,15 @@
 			// Variables to send via Ajax
 			var encoded_artist = encodeURIComponent(artist),
 			encoded_album      = encodeURIComponent(album),
-			acf_url            = 'http://ws.audioscrobbler.com/2.0/?method=album.getinfo&api_key=853b3e9d9f565707e7edd6f878c3d587&artist='+encoded_artist+'&album='+encoded_album+'&format=json',
+			lastfm_api_url     = 'http://ws.audioscrobbler.com/2.0/?method=album.getinfo&api_key=853b3e9d9f565707e7edd6f878c3d587&artist='+encoded_artist+'&album='+encoded_album+'&format=json',
 			html               = '';
 
 			clearSearch();
 
 			$.ajax({
 				type:'GET',
-				url: acf_url,
+				url: lastfm_api_url,
 				success : function(data){
-					// console.log(data.message);
 					$('#wait').hide();
 					if(data.message !== 'Artist not found' && data.message !== 'Album not found') {
 						if(thesize == 15) {
@@ -197,20 +201,21 @@
 						$('#setattachment').attr("disabled", false);
 						$('#insertineditor').attr("disabled", false);
 						$('#setposthtumbnail').attr("disabled", false);
-						$('#findalbum').val(prefix_object_name.search);
+						$('#findalbum').val(AlbumCoverFinderParams.search);
 						$('.clear').attr("disabled", false);
 
 					} else {
-						html += '<p>'+prefix_object_name.nofound+'</p>';
+						html += '<p>'+AlbumCoverFinderParams.nofound+'</p>';
 						$('#insertineditor').attr("disabled", true);
 						$('#setattachment').attr("disabled", true);
-						$('#findalbum').val(prefix_object_name.search);
+						$('#findalbum').val(AlbumCoverFinderParams.search);
 					}
 				},
 				complete: function(){
 					$('#cover').append(html).delay(200);
 					$('#album_info').fadeIn(200);
 					$('#cover img').eq(2).remove();
+					$('#findalbum').val(AlbumCoverFinderParams.search);
 				},
 				error : function(e,d,f){
 
